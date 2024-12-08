f7d95f9eedfa2e6ccb1ef1367294f1c4c0f862af
MithuAhammad <mithuahammad2@gmail.com>
2024-12-08 23:33:50 +0600

    fixed donation function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-donationHandle('')
+ 
+donationHandle('noakhali-donate-btn', 'noakhali-donate-amount', 'noakhali-amount', 'total-amount', 'history-section', 'noakhali-heading');
+ 
+donationHandle('feni-donate-btn', 'feni-donate-amount', 'feni-amount', 'total-amount', 'history-section', 'feni-heading');
+ 
+donationHandle('protest-donate-btn', 'protest-donate-amount', 'protest-amount', 'total-amount', 'history-section', 'quota-heading');
+
+document.getElementById('blog-btn')
+    .addEventListener('click', function () {
+        window.location.href = "blog.html";
+    });
