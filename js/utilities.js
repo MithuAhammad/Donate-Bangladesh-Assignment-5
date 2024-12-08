d26e442f40bf17af76882e16fdd3fafaa835c4f7
MithuAhammad <mithuahammad2@gmail.com>
2024-12-08 23:43:10 +0600

    added donation

--- js/utilities.js
+++ js/utilities.js
@@ -1,54 +1,46 @@
 function getInputAsNumber(id) {
-  const inputValue = document.getElementById(id).value;
-  const inputValueAsNumber = Number(inputValue);
-  return inputValueAsNumber;
+    const inputValue = document.getElementById(id).value;
+    const inputValueAsNumber = Number(inputValue);
+    return inputValueAsNumber;
 }
 
 function getTextAsNumber(id) {
-  const textValue = document.getElementById(id).innerText;
-  const textValueAsNumber = Number(textValue);
-  return textValueAsNumber;
+    const textValue = document.getElementById(id).innerText;
+    const textValueAsNumber = Number(textValue);
+    return textValueAsNumber;
 }
 
-function donationHandle(
-  buttonId,
-  donationInputNumberId,
-  totalDonationId,
-  totalAmountId,
-  historySection,
-  titleId
-) {
-  document.getElementById(buttonId).addEventListener("click", function () {
-    let donateAmount = getInputAsNumber(donationInputNumberId);
-    let currentDonationAmount = getTextAsNumber(totalDonationId);
-    let totalAmount = getTextAsNumber(totalAmountId);
+function donationHandle(buttonId, donationInputNumberId, totalDonationId, totalAmountId, historySection, titleId) {
+    document.getElementById(buttonId).addEventListener('click', function () {
+        let donateAmount = getInputAsNumber(donationInputNumberId);
+        let currentDonationAmount = getTextAsNumber(totalDonationId);
+        let totalAmount = getTextAsNumber(totalAmountId);
 
-    if (isNaN(donateAmount) || donateAmount <= 0) {
-      document.getElementById(donationInputNumberId).value = "";
-      alert("Invalid Donation Amount");
-      return;
-    }
+        if (isNaN(donateAmount) || donateAmount <= 0) {
+            document.getElementById(donationInputNumberId).value = '';
+            alert('Invalid Donation Amount');
+            return;
+        }
 
-    if (donateAmount > totalAmount) {
-      document.getElementById(donationInputNumberId).value = "";
-      alert("You don't have sufficient balance");
-      return;
-    }
+        if (donateAmount > totalAmount) {
+            document.getElementById(donationInputNumberId).value = '';
+            alert("You don't have sufficient balance");
+            return;
+        }
 
-    currentDonationAmount += donateAmount;
-    document.getElementById(totalDonationId).innerText =
-      currentDonationAmount.toFixed(2);
-    document.getElementById(donationInputNumberId).value = "";
+        currentDonationAmount += donateAmount;
+        document.getElementById(totalDonationId).innerText = currentDonationAmount.toFixed(2);
+        document.getElementById(donationInputNumberId).value = '';
 
-    totalAmount -= donateAmount;
-    document.getElementById(totalAmountId).innerText = totalAmount.toFixed(2);
+        totalAmount -= donateAmount;
+        document.getElementById(totalAmountId).innerText = totalAmount.toFixed(2);
 
-    let title = document.getElementById(titleId).textContent;
+        let title = document.getElementById(titleId).textContent;
 
-    let history = document.getElementById(historySection);
-    let newHistory = document.createElement("div");
+        let history = document.getElementById(historySection);
+        let newHistory = document.createElement('div');
 
-    newHistory.innerHTML = `
+        newHistory.innerHTML = `
             <div class="flex flex-col p-5 bg-white border border-border-color rounded-2xl">
                 <h3 class="text-lg font-bold">
                     ${donateAmount.toFixed(2)} Taka is ${title}
@@ -56,27 +48,27 @@ function donationHandle(
                 <p id="history-date" class="text-sm md:text-base text-paragraph-color">
                 </p>
             </div>
-            `;
+            `
 
-    history.insertBefore(newHistory, history.firstChild);
+        history.insertBefore(newHistory, history.firstChild);
 
-    const date = new Date();
-    newHistory.querySelector("#history-date").innerText =
-      "Date: " + date.toString();
+        const date = new Date();
+        newHistory.querySelector("#history-date").innerText = "Date: " + date.toString();
 
-    const displayModal = document.getElementById("my_modal_1");
-    displayModal.showModal();
-  });
-}
+        const displayModal = document.getElementById('my_modal_1');
+        displayModal.showModal();
+    });
+};
 
-const siteHeader = document.getElementById("header");
+
+const siteHeader = document.getElementById('header');
 
 function scrollHeaderStyle() {
-  if (window.scrollY > 50) {
-    siteHeader.classList.add("backdrop-blur-lg", "bg-white/30");
-  } else {
-    siteHeader.classList.remove("backdrop-blur-lg", "bg-white/30");
-  }
+    if (window.scrollY > 50) {
+        siteHeader.classList.add('backdrop-blur-lg', 'bg-white/30')
+    } else {
+        siteHeader.classList.remove('backdrop-blur-lg', 'bg-white/30')
+    };
 }
 
-window.addEventListener("scroll", scrollHeaderStyle);
+window.addEventListener('scroll', scrollHeaderStyle);
